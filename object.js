86580bb9fbab6bcfb6c2539a1ca1f849af2a5175
Dargis97 <dargismantas1@gmail.com>
2022-06-02 18:22:29 +0300

    obj

---
+++ object.js
@@ -0,0 +1,21 @@
+const person = {
+    name: 'Petras',
+    age: 99,
+    isMarried: true,
+    1: 1111,
+    'de mo' : 'demo'
+}
+
+console.log(person)
+
+console.log(person.name)
+console.log(person['name'])
+
+console.log(person.age)
+console.log(person['age'])
+
+console.log(person.isMarried)
+console.log(person['isMarried'])
+
+console.log(person[1])
+console.log(person['de mo'])
